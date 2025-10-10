619307c263b3b29b2ece0c8f230ed13e41e18874
potatoSalad21 <datuna.ziraqishvili@gmail.com>
2025-10-11 01:24:26 +0400

    Initial commit

---
+++ popup.js
@@ -0,0 +1,21 @@
+function displayErr(err) {
+    msg.style.color = "red";
+    msg.innerHTML = err;
+}
+
+search.onsubmit = (e) => {
+    e.preventDefault();
+    msg.innerHTML = "";
+
+    const cfPattern = /\d+[A-Z]/
+    const code = searchbar.value;
+
+    if (!cfPattern.test(code)) {
+        displayErr("Invalid CF problem code!");
+        return;
+    }
+
+    const num = code.slice(0, code.length-1);
+    const letter = code[code.length-1];
+    chrome.tabs.create({ url: `https://codeforces.com/problemset/problem/${num}/${letter}` });
+}
